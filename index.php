1bceaefbc58a13c7a93b0c4f10e44ed270bd5509
Lukas Wilkeer <lukaswilkeer@SonyVaio.(none)>
2013-08-01 20:36:50 -0300

    Mudanças no array de cervejas e acrescimo de mais uma rota.

--- index.php
+++ index.php
@@ -7,31 +7,44 @@ error_reporting(E_ALL);
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+
 //loader do composer
 $loader = require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Application();
 
 $cervejas = array(
-    'marcas' => array('Heineken','Guinness','Skol','Colorado'),
-    'estilos' => array('Pilsen','Stou')
-);
+    'Heineken' => array('estilo'=>'pilsen','nome'=>'heik'),
+    'Guinness' => array('estilo'=>'pilsen','nome'=>'Guinnes'),
+    'Skol' => array('estilo'=>'stou','nome'=>'Skol, a cerveja que desce redondo')
+    );
+
+$app->get('/cervejas',function () use ($cervejas){
+    //$result = implode(',',$cervejas['marcas']);
+    $result = $cervejas;
+    return new Response (json_encode($result),200);
+});
 
 $app->get('/cervejas/{id}', function ($id) use ($cervejas) {
     if($id == null){
-        return implode(',',$cervejas['marcas']);
+        return new Response (json_encode('Não existe o parametro ID'),404);
     }
 
-    $key = array_search($id,$cerveja['marcas']);
+    $key = array_key_exists($id,$cervejas); 
     if($key == null){
-        return 'Não encontrada';
+        return new Response (json_encode('Não encontrada'),404);
     }
-    return $cervejas['marcas'][$key];
+    return new Response (json_encode($cervejas[$id]),200);
 })->value('id',null);
 
 $app->get('/marcas', function () use ($cervejas){
     return implode(',',$cervejas['estilos']);
 });
 
+//Adiciona o cabeçalho após todas as requisições
+$app->after(function (Request $request, Response $response){
+    $response->headers->set('Content-Type','text/json');
+});
+$app['debug'] = true;
 $app->run();
 ?>
